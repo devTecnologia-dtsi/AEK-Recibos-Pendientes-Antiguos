9ebbe2b49c7399ef1d2438309a75e49fd12ede0f
LUIS DAVID MERCADO ORTEGA <luis.mercado.o@uniminuto.edu>
2025-11-26 10:48:14 -0500

    cambiando protocolo http a hhtps para recibos percunarios

--- src/helpers/serviciosUrl.js
+++ src/helpers/serviciosUrl.js
@@ -2,7 +2,7 @@
 
 export const urlObtenerRecibosPendientes = 'https://uniminuto.api.digibee.io/pipeline/uniminuto/v1/servicios-banner/obtenerRecibosPendientes';
 export const urlGetUserDetails = 'https://uniminuto.api.digibee.io/pipeline/uniminuto/v1/servicios-banner/getUserDetails';
-export const urlRutaBaseRecibosPendientes = 'http://www.uniminuto.edu/pecuniarios';
+export const urlRutaBaseRecibosPendientes = 'https://www.uniminuto.edu/pecuniarios';
 export const apikey='ITnjVcrLWfYpY2B246EcrWO6Hln3LD7a';
 export const apikey2 = 'ITnjVcrLWfYpY2B246EcrWO6Hln3LD7a';
 export const urlGenearIdPSE='https://uniminuto.api.digibee.io/pipeline/uniminuto/v1/pasarelas-pago/linkPagoZonaPagos'
@@ -12,7 +12,7 @@ export const urlPagoPSE= 'https://www.zonapagos.com/t_uniminuto/pago.asp?estado_
 //DATOS DE DESARROLLO
 // export const urlObtenerRecibosPendientes = 'https://uniminuto.test.digibee.io/pipeline/uniminuto/v1/servicios-banner/obtenerRecibosPendientes';
 // export const urlGetUserDetails = 'https://uniminuto.test.digibee.io/pipeline/uniminuto/v1/servicios-banner/getUserDetails';
-// export const urlRutaBaseRecibosPendientes = 'http://www.uniminuto.edu/pecuniariosqa';
+// export const urlRutaBaseRecibosPendientes = 'https://www.uniminuto.edu/pecuniariosqa';
 // export const apikey='5H9CcvkLZJTgPDDCXTXTI7KC90k6prl0';
 // export const apikey2 = 'uxpWFePgheXvuP9Tun8TYxvjb0FgeSLH';
 // export const urlGenearIdPSE= 'https://www.zonapagos.com/t_uniminutopasp/pago.asp?estado_pago=iniciar_pago&identificador=' //'https://uniminuto.test.digibee.io/pipeline/uniminuto/v1/pasarelas-pago/linkPagoZonaPagos'
